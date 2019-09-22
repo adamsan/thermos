6edf66dc60507810c8efe67a65f04e5df05b8a9a
Adam Szentirmai-Schon <adamsan3@gmail.com>
2019-09-22 22:38:39 +0200

    Add User class and data to display on the index page

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -4,11 +4,20 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+class User:
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
+
+
+    def initials(self):
+        return f"{self.firstname} {self.lastname}"
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    data = User('Albert', 'Einstein')
+    return render_template('index.html', data=data)
 
 
 if __name__ == '__main__':
