f921c427f2b1b9b606b738791d69e072a8546861
Adam Szentirmai-Schon <adamsan3@gmail.com>
2019-09-25 00:42:35 +0200

    Process add POST request, and redirect to index (post-redirect-get).

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -1,17 +1,31 @@
 #!/bin/python3
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+import logging
+import datetime
 
 app = Flask(__name__)
+app.logger.setLevel(logging.DEBUG)
+
+bookmarks = []
+
+
+def store_bookmark(url):
+    bookmarks.append({
+        'url': url,
+        'user': 'adamsan',
+        'date': datetime.datetime.utcnow()
+    })
+
 
 class User:
     def __init__(self, firstname, lastname):
         self.firstname = firstname
         self.lastname = lastname
 
-
     def initials(self):
         return f"{self.firstname} {self.lastname}"
+
 
 @app.route('/')
 @app.route('/index')
@@ -20,14 +34,20 @@ def index():
     return render_template('index.html', data=data)
 
 
-@app.route('/add')
+@app.route('/add', methods=['GET', 'POST'])
 def add():
+    if request.method == 'POST':
+        url = request.form['url']
+        store_bookmark(url)
+        app.logger.debug(f"stored url: {url}")
+        return redirect(url_for('index'))
     return render_template('add.html')
 
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
+
 
 @app.errorhandler(500)
 def server_error(e):
