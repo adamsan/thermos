765336af76a6a26be3839317724bc406001f02be
Adam Szentirmai-Schon <adamsan3@gmail.com>
2019-10-02 22:45:04 +0200

    Add many-to-one relationship between bookmarks and users. Modify create script to add default user, and fake logged in user

--- thermos/models.py
+++ thermos/models.py
@@ -10,6 +10,7 @@ class Bookmark(db.Model):
     url = db.Column(db.Text, nullable=False)
     date = db.Column(db.DateTime, default=datetime.now())
     description = db.Column(db.String(300))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return f"<Bookmark {self.description}: {self.url}>"
@@ -23,6 +24,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
+    bookmarks = db.relationship('Bookmark', backref='user', lazy='dynamic')
 
     def __repr__(self):
         return f"<User {self.username}>"
@@ -31,3 +33,16 @@ class User(db.Model):
 def create_db():
     print("Creating database...")
     db.create_all()
+    db.session.add(User(username='adam', email='adam@adam.net'))
+    db.session.add(User(username='peter', email='peter@adam.net'))
+    db.session.commit()
+# ipython
+# cd thermos
+# E:\flask_learn\flask_project\thermos
+# import thermos
+# import models
+# a = models.User.query.get(1)
+# for url,descr in [('http://www.prog.hu','HUN programmer site'),('http://www.index.hu','news site')]:
+#      thermos.db.session.add(models.Bookmark(user=a, url=url, description=descr))
+# thermos.db.session.commit()
+# a.bookmarks.all()
--- thermos/thermos.py
+++ thermos/thermos.py
@@ -21,9 +21,14 @@ app.logger.setLevel(logging.DEBUG)
 db = SQLAlchemy(app)
 
 
+# Fake login
+def logged_in_user():
+    return models.User.query.filter_by(username='adam').first()
+
+
 def store_bookmark(url, description):
     try:
-        bm = models.Bookmark(url=url, description=description)
+        bm = models.Bookmark(user=logged_in_user(), url=url, description=description)
         db.session.add(bm)
         db.session.commit()
     except OperationalError:
